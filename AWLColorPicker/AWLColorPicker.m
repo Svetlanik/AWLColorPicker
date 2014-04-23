9e3b737b99e483b85e1136c12770bce82af74df7
Svitlana Fedik <svitlana.fedik@gmail.com>
2014-04-24 01:46:22 +0300

    Fixed issue with nib file loading.

--- AWLColorPicker/AWLColorPicker.m
+++ AWLColorPicker/AWLColorPicker.m
@@ -9,7 +9,7 @@
 #import "AWLColorPicker.h"
 
 @interface NSBundle (AWLColorPicker)
-+ (BOOL)awl_loadNibNamed:(NSString*)aNibName owner:(id)anOwner;
++ (BOOL)awl_loadNibNamed:(NSString*)aNibName forBundleWithIdentifier:(NSString*)aBundleIdentifier owner:(id)anOwner;
 @end
 
 #pragma mark -
@@ -43,7 +43,8 @@
     if (initialRequest) {
         // Load our nib files
         static NSString *nibName = @"AWLColorPicker";
-        if (![NSBundle awl_loadNibNamed:nibName owner:self]) {
+        static NSString *bundleID = @"ua.com.wavelabs.AWLColorPicker";
+        if (![NSBundle awl_loadNibNamed:nibName forBundleWithIdentifier:bundleID owner:self]) {
             NSLog(@"ERROR: couldn't load %@ nib", nibName);
         }
     }
@@ -71,7 +72,7 @@
 #pragma mark -
 
 @implementation NSBundle (AWLColorPicker)
-+ (BOOL)awl_loadNibNamed:(NSString*)aNibName owner:(id)anOwner {
++ (BOOL)awl_loadNibNamed:(NSString*)aNibName forBundleWithIdentifier:(NSString*)aBundleIdentifier owner:(id)anOwner {
     /// loadNibNamed:owner:topLevelObjects was introduced in 10.8 (Mountain Lion).
     /// In order to support Lion and Mountain Lion +, we need to see which OS we're
     /// on. We do this by testing to see if [NSBundle mainBundle] responds to
@@ -82,9 +83,9 @@
     /// @see http://goo.gl/BFEQJd
     if ([[NSBundle mainBundle] respondsToSelector:@selector(loadNibNamed:owner:topLevelObjects:)]) {
         // We're running on Mountain Lion or higher
-        return [[NSBundle mainBundle] loadNibNamed:aNibName
-                                             owner:anOwner
-                                   topLevelObjects:nil];
+        return [[NSBundle bundleWithIdentifier:aBundleIdentifier] loadNibNamed:aNibName
+                                                                         owner:anOwner
+                                                               topLevelObjects:nil];
     } else {
         // We're running on Lion
         return [NSBundle loadNibNamed:aNibName
